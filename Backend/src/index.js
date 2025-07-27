86ce7e9cfeb2d29f387d00d2227060e5dbd75b85
Rakesh Nagarkar <rakeshpardeshi2004@gmail.com>
2025-07-27 15:37:20 +0530

    app.get() route change from '*' to '/{*any}'

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -35,7 +35,7 @@ app.use("/api/messages", messageRoutes)
 if(process.env.NODE_ENV == "production"){
 
     app.use(express.static(path.join(__dirname, "/frontend/build")));
-    app.get("*", (req, res) => {
+    app.get("/{*any}", (req, res) => {
         res.sendFile(path.join(__dirname, "/frontend/build/index.html"));
     });
 
