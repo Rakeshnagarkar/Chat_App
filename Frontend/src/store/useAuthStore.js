1936c79aaa587ac709cd105ac8dab92006cbfe00
Rakesh Nagarkar <rakeshpardeshi2004@gmail.com>
2025-07-27 17:38:51 +0530

    Backend url added in frontend

--- Frontend/src/store/useAuthStore.js
+++ Frontend/src/store/useAuthStore.js
@@ -5,7 +5,7 @@ import { io } from 'socket.io-client';
 
 export const useAuthStore = create((set, get) => ({
 
-    BASE_URL: import.meta.env.NODE_ENVV === "development" ? "http://localhost:5001" : '/',
+    BASE_URL: import.meta.env.NODE_ENV === "development" ? "http://localhost:5001" : process.env.BASE_URL,
 
     authUser: null,
     isSigningUp: false,
