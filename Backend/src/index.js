e62c4a2a963a1b25efc060e6b3f36bb302fea6c6
Rakesh Nagarkar <rakeshpardeshi2004@gmail.com>
2025-07-27 15:43:05 +0530

    typos in index.js fixed

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -34,12 +34,17 @@ app.use("/api/messages", messageRoutes)
 
 if(process.env.NODE_ENV == "production"){
 
-    app.use(express.static(path.join(__dirname, "/frontend/build")));
+    app.use(express.static(path.join(__dirname, "../../Frontend/dist")));
     app.get("/{*any}", (req, res) => {
-        res.sendFile(path.join(__dirname, "/frontend/build/index.html"));
+        res.sendFile(path.join(__dirname, "../../Frontend/dist/index.html"));
     });
 
 }
+
+// app.use(express.static(path.join(__dirname, "/frontend/build")));
+//     app.get("/{*any}", (req, res) => {
+//         res.sendFile(path.join(__dirname, "/frontend/build/index.html"));
+//     });
 
 app.get("/", (req, res) => {
     res.send("Backend is running! 🚀");
